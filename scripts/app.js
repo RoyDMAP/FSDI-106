05100cde48f902d0c6513cdb207e549cf762e667
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-07-17 20:03:38 -0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+function sayHello(){
+    console.log("hello there");
+}
+
+function init(){
+    sayHello();
+    console.log("Hello world");   
+}
+
+window.onload = sayHello;
+
+
+//DOM=Document Object Model
+//Client Server uses Hyper Text Transfer Protocol (HTTP) HTTPS: S=Secure
+//HTTP 1. Get 2. Post 3. Put 4. Patch 5. Delete
+//Get = Read something from the server
+//Post = Write something to the server
+//CRUD = Create  Read  Update  Delete
+//Time is very important, which is related to the size of files used in each project
+//Patch is used to update files
+//Put is used to replace eveything
+//SPA = Single Page App
+//API = Application Programming Interface
